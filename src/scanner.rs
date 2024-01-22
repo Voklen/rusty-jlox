a5087a866c8562a4dae0b69c825126ca66972a52
Alex Gorichev <Alex.Gorichev@protonmail.com>
2024-01-22 10:29:07 +0000

    Add one or two character tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,4 @@
-use std::str::Chars;
+use std::{iter::Peekable, str::Chars};
 
 use anyhow::Result;
 
@@ -12,7 +12,7 @@ pub struct Token {
     // literal
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum TokenType {
     // Single-character tokens
     LeftParen,
@@ -63,14 +63,33 @@ enum TokenType {
     EOF,
 }
 
-pub fn scanner(string: Chars) -> Result<impl Iterator<Item = Token> + '_> {
-    let res = string.filter_map(scan_token);
-    Ok(res)
+pub fn scanner(chars: Chars) -> Result<Vec<Token>> {
+    let mut tokens = vec![];
+    let mut peekable = chars.peekable();
+    loop {
+        let token = scan_token(&mut peekable);
+        match token {
+            Some(token) => {
+                if token.token_type == TokenType::EOF {
+                    break;
+                }
+                tokens.push(token)
+            }
+            None => {}
+        }
+    }
+    Ok(tokens)
 }
 
-fn scan_token(char: char) -> Option<Token> {
+fn scan_token(chars: &mut Peekable<Chars>) -> Option<Token> {
+    let next_char = chars.next();
+    let char = match next_char {
+        Some(char) => char,
+        None => return Some(add_token(EOF, "")),
+    };
     use TokenType::*;
     let token = match char {
+        // Single-character tokens
         '(' => add_token(LeftParen, "("),
         ')' => add_token(RightParen, ")"),
         '{' => add_token(LeftBrace, "{"),
@@ -81,6 +100,12 @@ fn scan_token(char: char) -> Option<Token> {
         '+' => add_token(Plus, "+"),
         ';' => add_token(Semicolon, ";"),
         '*' => add_token(Star, "*"),
+        // One or two character tokens
+        '!' => add_eq_token(Bang, BangEqual, "!", chars),
+        '=' => add_eq_token(Equal, EqualEqual, "=", chars),
+        '<' => add_eq_token(Less, LessEqual, "<", chars),
+        '>' => add_eq_token(Greater, GreaterEqual, ">", chars),
+        '/' => slash(chars)?,
         unexpected => {
             error!("Unexpected character: {unexpected}");
             return None;
@@ -95,4 +120,31 @@ fn add_token(token_type: TokenType, lexeme: &str) -> Token {
         lexeme: lexeme.to_string(),
         line: 1,
     };
+}
+
+fn add_eq_token(
+    base_type: TokenType,
+    eq_type: TokenType,
+    base_lexeme: &str,
+    chars: &mut Peekable<Chars>,
+) -> Token {
+    if chars.peek() == Some(&'=') {
+        add_token(eq_type, &format!("={base_lexeme}"))
+    } else {
+        add_token(base_type, base_lexeme)
+    }
+}
+
+fn slash(chars: &mut Peekable<Chars>) -> Option<Token> {
+    if chars.peek() == Some(&'/') {
+        while !reached_newline(chars) {}
+        return None;
+    };
+    let token = add_token(TokenType::Slash, "/");
+    Some(token)
+}
+
+fn reached_newline(chars: &mut Peekable<Chars>) -> bool {
+    let next_char = chars.next();
+    next_char == None || next_char == Some('\n')
 }
